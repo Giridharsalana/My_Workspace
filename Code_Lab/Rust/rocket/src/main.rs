e7619a6a7ae0a849bce6d40642b25fdc80e05691
Giridhar Salana <giridharsalana@gmail.com>
2022-10-06 18:56:59 +0000

    Updated  Repo

--- Code_Lab/Rust/rocket/src/main.rs
+++ Code_Lab/Rust/rocket/src/main.rs
@@ -1,11 +1,18 @@
 use std::collections::HashMap;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn req() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::get("https://httpbin.org/ip")
         .await?
         .json::<HashMap<String, String>>()
         .await?;
     println!("{:#?}", resp);
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    one();
+    let one = req().await;
+    println!("{:?}", one)
+    
 }
